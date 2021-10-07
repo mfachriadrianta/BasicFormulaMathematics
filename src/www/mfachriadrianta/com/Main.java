6c4649f9206c8aff88764af4cc57f3fef302a941
Pai <muhammadfachriadrianta12@gmail.com>
2021-10-07 18:14:06 +0700

    Add

--- src/www/mfachriadrianta/com/Main.java
+++ src/www/mfachriadrianta/com/Main.java
@@ -8,11 +8,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        checkingData();
-
+        checkingLogin();
     }
 
-    private static void checkingData() throws IOException{
+    private static void checkingLogin() throws IOException{
 
         FileReader fileReader;
         BufferedReader bufferedReader;
@@ -25,38 +24,26 @@ public class Main {
 
             while (next){
                 clearScreen();
-                System.out.println("");
-                System.out.println("Login");
-                Scanner systemInput = new Scanner(System.in);
-                String edt_username, edt_password;
-                System.out.print("Username : ");
-                edt_username = systemInput.nextLine();
-                System.out.print("Password : ");
-                edt_password = systemInput.nextLine();
+            System.out.println("");
+            System.out.println("Login");
+            Scanner systemInput = new Scanner(System.in);
+            String edt_username, edt_password;
+            System.out.print("Username : ");
+            edt_username = systemInput.nextLine();
+            System.out.print("Password : ");
+            edt_password = systemInput.nextLine();
 
                 if(!edt_username.equalsIgnoreCase(dataUsername) && !edt_password.equalsIgnoreCase(dataPassword)){
                     System.out.println("Login failed!");
+                    next = repeatLogin("Are you want next ? ");
                 } else {
-
+                    dashboardMenu();
+                    next = false;
                 }
-
-                next = repeatLogin("Are you want next ? ");
             }
             }catch (Exception e){
             System.err.println("Nothing data");
             }
-    }
-
-    private static void clearScreen(){
-        try {
-            if (System.getProperty("os.name").contains("Windows")){
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.println("\033\143");
-            }
-        }catch (Exception e){
-            System.err.println("Can not proccess clear system!");
-        }
     }
 
     private static boolean repeatLogin(String message){
@@ -71,5 +58,32 @@ public class Main {
         }
 
         return inputUser.equalsIgnoreCase("Yes");
+    }
+
+    private static void dashboardMenu() throws IOException{
+        Scanner inputMenu = new Scanner(System.in);
+        String inputUserMenu = inputMenu.nextLine();
+        boolean next = true;
+
+        while (next){
+
+            System.out.println("1 . Calculation Fragment");
+            System.out.println("2. Calculation ");
+//            switch (){
+//
+//            }
+        }
+
+    }
+    private static void clearScreen(){
+        try {
+            if (System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.println("\033\143");
+            }
+        }catch (Exception e){
+            System.err.println("Can not proccess clear system!");
+        }
     }
 }
