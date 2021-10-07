218d2e6ee321b6904a5597f79d1d745ac38cea8d
M Fachri Adrianta <59374440+mfachriadrianta@users.noreply.github.com>
2021-10-07 15:59:12 +0700

    Delete Dashboard.java

--- src/www/mfachriadrianta/com/Dashboard.java
+++
@@ -1,9 +0,0 @@
-package www.mfachriadrianta.com;
-
-import java.io.IOException;
-
-public class Dashboard {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Dashboard");
-    }
-}
